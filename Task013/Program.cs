9b37edcb17632ae51cd72acf10de09e0944efd87
davydova <natalya-davydova-86@mail.ru>
2023-05-01 17:31:09 +0300

    Added Task 013

---
+++ Task013/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №13
+// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = 0;
+if (A > 99 && A < 1000)
+{
+    B = A % 10;
+    Console.WriteLine(B);
+}
+else if (A / 1000 > 0 && A / 1000 < 10)
+{
+    B = (A % 100)/10;
+    Console.WriteLine(B);
+}
+else if (A / 1000 > 10 && A / 1000 < 10)
+{
+    A = (A % 1000)/100;
+    Console.WriteLine(A);
+}
+else if (A / 1000 > 100 && A / 1000 < 1000)
+{
+    B = (A % 10000)/1000;
+    Console.WriteLine(B);
+
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
